1773cd3c0ece946256828b4d25231961d0007ada
Isaac Oduh <ufuomaoduh@gmail.com>
2019-02-07 05:56:58 +0100

    Updates

---
+++ precache-manifest.57ab061001027e54c12dd7c008666203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "9550c8b61962efc28a2c",
+    "url": "/me/static/js/main.9550c8b6.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/me/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "9550c8b61962efc28a2c",
+    "url": "/me/static/css/main.b32c92ae.chunk.css"
+  },
+  {
+    "revision": "4bc5564f8ffa5ad78c19d3c2cee9221b",
+    "url": "/me/index.html"
+  }
+];
